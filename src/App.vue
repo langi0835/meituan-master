97786b6ca9d1c9aaf0b36806a9735737ffb39592
unknown <yuki_lan@bankpro.com.tw>
2018-02-03 15:56:22 +0800

    商品頁捲動功能完成

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,53 @@
 <template>
-  <div id="app">
+	<div id="app">
 
-    <!-- header -->
-    <Myheader :poiInfo='poiInfo'></Myheader>
+		<!-- header -->
+		<headerComponent :poiInfo='poiInfo'></headerComponent>
 
-    <!-- nav -->
-    <Mynav></Mynav>
+		<!-- nav -->
+		<navComponent></navComponent>
 
-    <!-- content -->
-    <router-view></router-view>
-  </div>
+		<!-- content -->
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-  import Myheader from 'components/Header/Header';
-  import Mynav from 'components/Nav/Nav';
+	import headerComponent from 'components/Header/Header';
+	import navComponent from 'components/Nav/Nav';
 
-  export default {
-    name: "App",
-    components: {
-      Myheader, Mynav
-    },
-    data() {
-      return {
-        //header需要的json資料
-        poiInfo: {}
-      }
-    },
-    //可在此進行非同步獲取資料
-    created() {
-      var that = this;
+	export default {
+		name: "App",
+		components: {
+			headerComponent,
+			navComponent
+		},
+		data() {
+			return {
+				//header需要的json資料
+				poiInfo: {}
+			}
+		},
+		//可在此進行非同步獲取資料
+		created() {
+			var that = this;
 
-      //因為已經將axios設為vue原型,所以可以用this(vue)呼叫
-      this.$axios.get('/api/goods')
-        .then(function (response) {//獲取數據
-          //console.log(response);
-          var dataSourse = response.data;
-          //console.log(dataSourse);
-          if(dataSourse.code == 0)
-          {
-            that.poiInfo = dataSourse.data.poi_info;
-            console.log(that.poiInfo);
-          }
-        })
-        .catch(function (error) { //錯誤處理
-          console.log(error);
-        });
-    }
-  };
+			//因為已經將axios設為vue原型,所以可以用this(vue)呼叫
+			this.$axios.get('/api/goods')
+				.then(function (response) {//獲取數據
+					//console.log(response);
+					var dataSourse = response.data;
+					//console.log(dataSourse);
+					if (dataSourse.code == 0) {
+						that.poiInfo = dataSourse.data.poi_info;
+						//console.log(that.poiInfo);
+					}
+				})
+				.catch(function (error) { //錯誤處理
+					console.log(error);
+				});
+		}
+	};
 </script>
 
 <style>
--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -13,48 +13,46 @@
       商家
       <i class="line"></i>
     </router-link>
-    
+
   </div>
 </template>
 
 <script>
-  export default{
-
-  }
+export default {};
 </script>
 
 <style>
-  .nav{
-    display: flex;
-    width: 100%;
-    height: 40px;
-    line-height: 40px;
-    border-bottom: 1px solid #e4e4e4;
-  }
+.nav {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  border-bottom: 1px solid #e4e4e4;
+}
 
-  .nav .nav-item{
-    flex: 1;
-    text-align: center;
-    font-size: 13px;
-    text-decoration:none;
-    color: #666666;
-    position: relative;    
-  }
+.nav .nav-item {
+  flex: 1;
+  text-align: center;
+  font-size: 13px;
+  text-decoration: none;
+  color: #666666;
+  position: relative;
+}
 
-  /* active router點選後的class */
-  .nav .active{
-    color: #ffbb22;
-  }
+/* active router點選後的class */
+.nav .active {
+  color: #ffbb22;
+}
 
-  .nav .active .line{
-    width: 20px;
-    height: 2px;
-    display: inline-block;
-    background-color: #ffbb22;
+.nav .active .line {
+  width: 20px;
+  height: 2px;
+  display: inline-block;
+  background-color: #ffbb22;
 
-    position: absolute;
-    left: 50%;
-    bottom: 0;
-    margin-left: -10px;
-  }
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  margin-left: -10px;
+}
 </style>
